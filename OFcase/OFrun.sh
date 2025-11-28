dfb61513a12fb27abd46f20e5b9d2f78e5d9afbf
Manoj Gangadhar <manojgowda2805@gmail.com>
2025-11-28 13:06:39 +0100

    Make OFrun.sh usable both locally and on cluster (selectable run mode)

--- OFcase/OFrun.sh
+++ OFcase/OFrun.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-srun simpleFoam -parallel > log &
+
+mpirun -np $1 simpleFoam -parallel > log &      #to run in on local machine
+#srun -n $1 simpleFoam -parallel > log &  #to run on clusters either interactive node or sbatch
+
 wait $!
